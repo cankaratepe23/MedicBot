c8bc767b729f4af7d2f79b69c0051ad72f9c6b49
Can Karatepe <cankaratepe2000@gmail.com>
2022-03-04 17:49:15 +0300

    Replace collection logic with tag logic.

--- MedicBot/Model/AudioTrack.cs
+++ MedicBot/Model/AudioTrack.cs
@@ -5,10 +5,9 @@ namespace MedicBot.Model;
 public class AudioTrack
 {
     public ObjectId Id { get; set; }
-    public string AudioName { get; set; }
+    public string Name { get; set; }
     public List<string> Aliases { get; set; }
+    public List<string> Tags { get; set; }
     public string Path { get; set; }
     public ulong OwnerId { get; set; }
-
-    [BsonRef("AudioCollection")] public List<AudioCollection> AudioCollections { get; set; }
 }
