95f9d58a4f35e06b0ac874d93814c1173a800ea9
Can Karatepe <cankaratepe2000@gmail.com>
2022-03-22 16:10:42 +0300

    WIP Create argument converter for Attachments

---
+++ MedicBot/Utils/DiscordAttachmentConverter.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.Entities;
+
+namespace MedicBot.Utils;
+
+public class DiscordAttachmentConverter : IArgumentConverter<DiscordAttachment>
+{
+    public Task<Optional<DiscordAttachment>> ConvertAsync(string value, CommandContext ctx)
+    {
+        throw new NotImplementedException();
+    }
+}
