e59ba63f0c225a25a6d1ca6ce599cbf647959039
Can Karatepe <cankaratepe2000@gmail.com>
2023-10-29 03:31:43 +0300

    WIP Create Image commands and manager

---
+++ MedicBot/Commands/ImageReplyCommands.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using MedicBot.Utils;
+
+namespace MedicBot;
+
+[Group("image")]
+public class ImageReplyCommands : BaseCommandModule
+{
+    [Command("add")]
+    public async Task AddCommand(CommandContext ctx, [RemainingText] string imageName)
+    {
+        try
+        {
+            await ImageManager.AddAsync(imageName, ctx.Message.Author.Id, ctx.Message.GetFirstAttachment().Url);
+        }
+        catch (Exception e)
+        {
+            await ctx.RespondAsync(e.Message);
+            return;
+        }
+
+        await ctx.Message.RespondThumbsUpAsync();
+    }
+}
---
+++ MedicBot/Manager/ImageManager.cs
@@ -0,0 +1,48 @@
+﻿using MedicBot.Utils;
+using Serilog;
+
+namespace MedicBot;
+public static class ImageManager
+{
+    public static async Task AddAsync(string imageName, ulong userId, string url)
+    {
+        // TODO: De-duplicate this code
+        // Middleware/pre-processor to handle incoming files?
+        // File downloader?
+        // Some kind of repository interface to call .NameExists() without knowing whether incoming file is AudioTrack or Image.
+        // OR : Generic class/methods to do this
+        
+        if (!imageName.IsValidFileName())
+        {
+            Log.Warning("{Filename} has invalid characters", imageName);
+            throw new ArgumentException($"Filename: {imageName} has invalid characters.");
+        }
+
+        if (url.LastIndexOf('.') == -1 || string.IsNullOrWhiteSpace(url[url.LastIndexOf('.')..]))
+        {
+            Log.Warning("Discord attachment doesn't have a file extension");
+            throw new ArgumentException(
+                "The file you sent has no extension. Please add a valid extension to the file before sending it.");
+        }
+
+        if (ImageRepository.NameExists(imageName))
+        {
+            Log.Warning("An Image with the name {imageName} already exists", imageName);
+            throw new ImageExistsException($"An Image with the name {imageName} already exists.");
+        }
+
+        var fileExtension = url[url.LastIndexOf('.')..];
+        Log.Information("Detected file extension: {FileExtension}", fileExtension);
+        var fileName = imageName + fileExtension;
+        var filePath = string.Join('/', ImagesPath, fileName);
+
+        {
+            Log.Information("Downloading file to {FilePath}", filePath);
+            using var client = new HttpClient();
+            await using var stream = await client.GetStreamAsync(url);
+            await using var fileStream = File.OpenWrite(filePath);
+            await stream.CopyToAsync(fileStream);
+            await fileStream.FlushAsync();
+        }
+    }
+}
