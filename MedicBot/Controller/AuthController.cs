79ee1ae1159f11e18361368c75586b3864eeab1b
Can Karatepe <cankaratepe2000@gmail.com>
2023-12-17 00:56:11 +0300

    Add parameter hint

--- MedicBot/Controller/AuthController.cs
+++ MedicBot/Controller/AuthController.cs
@@ -10,7 +10,7 @@ public class AuthController : ControllerBase
     [HttpGet("TestLogin")]
     public async Task<IActionResult> EmulateLoginFromFrontend()
     {
-        return Challenge(new AuthenticationProperties {RedirectUri = "/swagger/index.html"}, "Discord");
+        return Challenge(new AuthenticationProperties {RedirectUri = "/swagger/index.html"}, authenticationSchemes: "Discord");
     }
 
     [HttpGet("DiscordLogin")]
